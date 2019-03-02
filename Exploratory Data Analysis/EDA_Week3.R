60bfdbd734bde44340999929c171038fe8ddb628
Piotr <piotr.patrzalek94@gmail.com>
2019-03-02 17:05:58 +0100

    Add Lesson 3

--- Exploratory Data Analysis/EDA_Week3.R
+++ Exploratory Data Analysis/EDA_Week3.R
@@ -102,3 +102,48 @@ par(mfrow = c(1, 3))
 image(t(dataMatrix)[, nrow(dataMatrixOrdered):1])
 plot(rowMeans(dataMatrixOrdered), 40:1, xlab = "Row Mean", ylab = "Row", pch = 19)
 plot(colMeans(dataMatrixOrdered), xlab = "Column Mean", ylab = "Column", pch = 19)
+
+# Components of the SVD - v and u
+
+svd1 <- svd(scale(dataMatrixOrdered))
+par(mfrow = c(1, 3))
+image(t(dataMatrixOrdered)[,nrow(dataMatrixOrdered):1])
+plot(svd1$u[,1], 40:1, xlab = "Row", ylab = "First left singular vector", pch = 19)
+plot(svd1$v[,1],  xlab = "Column", ylab = "First right singular vector", pch = 19)
+
+# Components of the SVD - Variance explained
+par(mfrow = c(1, 2))
+plot(svd1$d, xlab = "Column", ylab = "Singular value", pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Prop. of variance explained", pch = 19)
+
+# Relationship to principal component
+svd1 <- svd(scale(dataMatrixOrdered))
+pca1 <- prcomp(dataMatrixOrdered, scale = TRUE)
+plot(pca1$rotation[,1], svd1$v[,1], pch = 19, xlab = "Principal Component 1", ylab = "Right Singular Vector 1")
+abline(c(0, 1))
+
+# Components of the SVD - variance explained
+constantMatrix <- dataMatrixOrdered*0
+for(i in 1:dim(dataMatrixOrdered)[1]){constantMatrix[i,] <- rep(c(0, 1), each = 5)}
+svd1 <- svd(constantMatrix)
+par(mfrow = c(1,3))
+image(t(constantMatrix)[,nrow(constantMatrix):1])
+plot(svd1$d, xlab = "Column", ylab = "Singular value", pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Prop. of variance explained", pch = 19)
+
+# Missing value
+dataMatrix2 <- dataMatrixOrdered
+
+dataMatrix2[sample(1:100, size = 40, replace = FALSE)] <- NA
+svd1 <- svd(scale(dataMatrix2))
+
+# Lesson 3: Working with colors
+pal <- colorRamp(c("red", "blue"))
+pal(1)
+pal(0)
+pal(0.5)
+pal(seq(0, 1, len = 10))
+
+pal <- colorRampPalette(c("red", "yellow"))
+pal(2)
+pal(10)
